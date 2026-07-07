0727bc83f7b5ce61dd519e14c1e188a6d7069464
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-07-07 22:18:59 +0530

    Update build files & gitignore

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,31 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import java.util.Properties
+
 plugins {
     kotlin("jvm")
     `maven-publish`
+    id("com.vanniktech.maven.publish")
 }
 
-group = "dev.justincodinguk.safeexports"
+group = "io.github.justincodinguk.safeexports"
+val projectArtifactId = "kdoc-export-ts-processor"
 version = "1.0.0"
+
+val sonatypeProperties = Properties().apply {
+    val file = rootProject.file("central.sonatype.properties")
+    if(file.exists()) {
+        file.inputStream().use { load(it) }
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+sonatypeProperties.forEach { (key, value) ->
+    project.extensions.extraProperties[key as String] = value
+}
 
 repositories {
     mavenCentral()
@@ -12,6 +33,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.3.0")
     testImplementation(kotlin("test"))
 }
 
@@ -23,10 +45,33 @@ tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates(group.toString(), projectArtifactId, version.toString())
+    pom {
+        name.set("SafeExports Kotlin/JS KDoc Embedder")
+        description.set("A Gradle plugin which embeds your KDoc inside the generated typescript bindings")
+        url.set("https://github.com/justincodinguk/kdoc-export-ts")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("justincodinguk")
+                name.set("Justin")
+                email.set("coderstudent09@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/justincodinguk/kdoc-export-ts.git")
+            developerConnection.set("scm:git:ssh://github.com/justincodinguk/kdoc-export-ts.git")
+            url.set("https://github.com/justincodinguk/kdoc-export-ts.git")
         }
     }
 }
